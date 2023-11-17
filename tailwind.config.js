1feb1373692cf77828e9d75a25787a33dd17185c
Fred Bradley <code@fredbradley.co.uk>
2023-11-17 14:35:42 +0000

    With style and written content

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "./index.html"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'senior': '#0C223f',
+            }
+        },
+    },
+    plugins: [],
+}
+
